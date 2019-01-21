95d3cfe86f4f04c62bb822bbd225d755a3e6dcb5
Katherine Shevchenko <pochta13524@gmail.com>
2019-01-21 11:23:31 +0200

    Add navbar-component admin-navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminNavBarComponent } from './admin-nav-bar/admin-nav-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AdminNavbarComponent } from './admin-navbar/admin-navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminNavBarComponent
+    AdminNavBarComponent,
+    AdminNavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
